4f16a0582c903bb599825a410e1e09c1fd903e5b
Kirill Shantin <kiril07.2002@mail.ru>
2024-06-26 12:05:31 +0600

    Added logger

--- go.mod
+++ go.mod
@@ -1,3 +1,17 @@
 module prog-grpcls
 
 go 1.21.4
+
+require github.com/pingcap/log v1.1.0
+
+require (
+	github.com/BurntSushi/toml v1.2.1 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.5.0 // indirect
+	github.com/joho/godotenv v1.5.1 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.19.0 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
--- main.go
+++ main.go
@@ -1,11 +1,49 @@
-package sso
+package main
+
+import (
+	"log/slog"
+	"os"
+
+	"prog-grpcls/internal/config"
+)
+
+const (
+	envLocal = "local"
+	envDev   = "dev"
+	envProd  = "prod"
+)
 
 func main() {
 	// TODO: инит объект конфига
-
+	cfg := config.MustLoad()
+	log := setupLogger(cfg.Env)
+	log.Info("starting app...",
+		slog.String("env", cfg.Env),
+		slog.Any("env", cfg),
+		slog.Int("env", cfg.GRPC.Port),
+	)
 	// TODO: инит логгера
 
 	// TODO: инит приложения
 
 	// TODO: запустить gRPC-сервер приложения
+}
+
+func setupLogger(env string) *slog.Logger {
+	var log *slog.Logger
+	switch env {
+	case envLocal:
+		log = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envDev:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case envProd:
+		log = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+	}
+	return log
 }
